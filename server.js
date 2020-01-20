60c4de26256227030ee8ee381bb114feec3c01ed
박태원 <producer82@naver.com>
2020-01-20 18:58:09 +0900

    웹서버 구현

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var http = require("http");
+var url = require("url");
+//TODO = fs를 리퀘스트 핸들러 또는 라우터로 이동 후 start에서 데이터만 받게 수정할 것
 var fs = require("fs");
 
-function start(){
+function start(route){
     http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        var pathname = url.parse(req.url).pathname;
+        console.log(pathname + ' 으로부터의 요청 접수됨');
+        route(pathname);
+        
         fs.readFile('index.html', (err, data) => {
             if (err){
                 return console.err(err);
